d4ba426b3100b68fd8a554023b506d4fb9e63fde
brian <brian@bigdog.domain>
2014-12-07 23:47:41 -0600

    Pipeline only the custom content

--- ActiveRules/tasks/pipeline.js
+++ ActiveRules/tasks/pipeline.js
@@ -15,6 +15,7 @@
 // (if you're using LESS with the built-in default config, you'll want
 //  to change `assets/styles/importer.less` instead.)
 var cssFilesToInject = [
+  'bower_components/bootstrap/dist/css/bootstrap.min.css',
   'styles/**/*.css'
 ];
 
@@ -27,11 +28,17 @@ var jsFilesToInject = [
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/**/*.js',
+  //'js/dependencies/**/*.js',
 
   // All of the rest of your client-side js files
   // will be injected here in no particular order.
-  'js/**/*.js'
+  //'js/**/*.js',
+  
+  // Bower components
+  //'bower_components/jquery/dist/jquery.min.js',
+  //'bower_components/angular/angular.min.js',
+  //'bower_components/bootstrap/dist/js/bootstrap.min.js' 
+  
 ];
 
 
