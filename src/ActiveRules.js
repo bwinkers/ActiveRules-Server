50ed4a687c59ce70306d45b8569c81d08dbb4324
Brian Winkers <bwinkers@gmail.com>
2018-07-23 22:18:58 -0700

    base extension of rules class

--- src/ActiveRules.js
+++ src/ActiveRules.js
@@ -1,6 +1,14 @@
+'use strict'
+
+const { Fact, Rule, Operator, Engine } = require( 'json-rules-engine');
+
 class ActiveRules {
-  
+  constructor () {
+    this.Fact = Fact;
+    this.Rule = Rule;
+    this.Operator = Operator;
+    this.Engine = Engine;
+  }
 }
 
-export default ActiveRules;
-
+module.exports = ActiveRules
