ac4c46cf46c6f27bdc26cbbd4a2d37989cc2d0ed
Brian Winkers <bwinkers@gmail.com>
2018-07-24 23:32:33 -0700

    Add JSON Validator based on djv

--- src/ActiveRules.js
+++ src/ActiveRules.js
@@ -2,12 +2,15 @@
 
 const { Fact, Rule, Operator, Engine } = require('json-rules-engine')
 
+const djv = require('djv');
+
 class ActiveRules {
   constructor () {
     this.Fact = Fact
     this.Rule = Rule
     this.Operator = Operator
     this.Engine = Engine
+    this.Validator = djv;
   }
 }
 
