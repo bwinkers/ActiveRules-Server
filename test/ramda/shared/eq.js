66681865b4b18d4b5cd476f7715d44340393dadf
Brian Winkers <bwinkers@gmail.com>
2018-07-28 15:35:31 -0700

    Simplify tests

--- test/ramda/shared/eq.js
+++
@@ -1,9 +0,0 @@
-const ActiveRules = require('../../../src/ActiveRules.js')
-const AR = new ActiveRules()
-const R = AR.R;
-const assert = require('assert');
-
-module.exports = function(actual, expected) {
-  assert.strictEqual(arguments.length, 2);
-  assert.strictEqual(R.toString(actual), R.toString(expected));
-};
