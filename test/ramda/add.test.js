6cd937d47b5aa815d866812bb53d6a1d24a0d542
Brian Winkers <bwinkers@gmail.com>
2018-07-28 17:14:31 -0700

    linted

--- test/ramda/add.test.js
+++ test/ramda/add.test.js
@@ -2,31 +2,28 @@
 
 const ActiveRules = require('../../src/ActiveRules.js')
 const AR = new ActiveRules()
-const R = AR.R;
-const assert = require('assert');
-const jsv = require('jsverify');
+const R = AR.R
+const assert = require('assert')
+const jsv = require('jsverify')
 
 describe('R', () => {
-  
-  describe('add', function() {
-    it('adds together two numbers', function() {
-      assert.equal(R.add(3, 7), 10);
-    });
+  describe('add', function () {
+    it('adds together two numbers', function () {
+      assert.equal(R.add(3, 7), 10)
+    })
+  })
 
-  });
-  
-  describe('add properties', function() {
-    jsv.property('commutative', jsv.number, jsv.number, function(a, b) {
-      return R.add(a, b) === R.add(b, a);
-    });
+  describe('add properties', function () {
+    jsv.property('commutative', jsv.number, jsv.number, function (a, b) {
+      return R.add(a, b) === R.add(b, a)
+    })
 
-    jsv.property('associative', jsv.number, jsv.number, jsv.number, function(a, b, c) {
-      return R.add(a, R.add(b, c)) === R.add(R.add(a, b), c);
-    });
+    jsv.property('associative', jsv.number, jsv.number, jsv.number, function (a, b, c) {
+      return R.add(a, R.add(b, c)) === R.add(R.add(a, b), c)
+    })
 
-    jsv.property('identity', jsv.number, function(a) {
-      return R.add(a, 0) === a && R.add(0, a) === a;
-    });
-  });
-
+    jsv.property('identity', jsv.number, function (a) {
+      return R.add(a, 0) === a && R.add(0, a) === a
+    })
+  })
 })
