02538a3b68decd07c650f77df13e43967b9d9925
Brian Winkers <bwinkers@gmail.com>
2018-07-29 15:41:12 -0700

    Activity class

---
+++ src/Activity.js
@@ -0,0 +1,11 @@
+/**
+ * An ActiveRules Activity defines a process and sets of rules around.
+ */
+
+const ARObject = require('./ActiveRulesObject.js')
+
+class Activity extends ARObject {
+  
+}
+
+module.exports = Activity;
