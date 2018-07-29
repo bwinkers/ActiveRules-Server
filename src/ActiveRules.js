f6545565688c76a061f07c51a008efa71273cbae
Brian Winkers <bwinkers@gmail.com>
2018-07-29 15:08:26 -0700

    use arError

--- src/ActiveRules.js
+++ src/ActiveRules.js
@@ -1,9 +1,9 @@
 'use strict'
 
 const { Fact, Rule, Operator, Engine } = require('json-rules-engine')
-
 const djv = require('djv')
 const R = require('ramda')
+const arError = require('./errors/ARError.js')
 
 class ActiveRules {
   
@@ -14,6 +14,7 @@ class ActiveRules {
     this.Engine = Engine
     this.Validator = djv
     this.R = R
+    this.arError = arError
   }
   
   /**
@@ -22,13 +23,21 @@ class ActiveRules {
    * 
    * @param {Object} rules - An ActiveRules JSON rule definition
    * @param {Object} data - ActiveRules JSON data object 
-   * @return {Object} rulesResult - ActiveRules rulesResult JSON object
+   * 
+   * @return {Object} rulesResult - ActiveRules RulesResult JSON object
    * @return {Object} rulesResult.data - modified, validated data
-   * @return {Object} rulesResult.errors - ActiveRules errors object
+   * @return {Object} rulesResult.errors - ActiveRules Errors object
    * @return {Object} rulesResult.actions - ActiveRules Actions object
    */
   rulesResult(rules, data) {
-    
+  
+    /**
+     * Validate, scrub and transform data.
+     * Create errors and stop processing if invalid.
+     * 
+     * Process Rules and create actions and or errors.
+     * Return RulesResults object.
+     */
 
   }
 }
