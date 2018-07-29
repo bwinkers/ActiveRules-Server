f18d27daa8b14cc884d30cc822cc42fad9d0a76b
Brian Winkers <bwinkers@gmail.com>
2018-07-28 17:13:22 -0700

    Stubbed out

--- src/ActiveRules.js
+++ src/ActiveRules.js
@@ -6,6 +6,7 @@ const djv = require('djv')
 const R = require('ramda')
 
 class ActiveRules {
+  
   constructor () {
     this.Fact = Fact
     this.Rule = Rule
@@ -15,7 +16,21 @@ class ActiveRules {
     this.R = R
   }
   
-  
+  /**
+   * Process a set of data using provided rules.
+   * An ActiveRules rulesResult object set will be returned. 
+   * 
+   * @param {Object} rules - An ActiveRules JSON rule definition
+   * @param {Object} data - ActiveRules JSON data object 
+   * @return {Object} rulesResult - ActiveRules rulesResult JSON object
+   * @return {Object} rulesResult.data - modified, validated data
+   * @return {Object} rulesResult.errors - ActiveRules errors object
+   * @return {Object} rulesResult.actions - ActiveRules Actions object
+   */
+  rulesResult(rules, data) {
+    
+
+  }
 }
 
 module.exports = ActiveRules
