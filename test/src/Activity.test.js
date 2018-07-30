8ddf43efd62d999e3fe6c5b7ca4303a2a60632f2
Brian Winkers <bwinkers@gmail.com>
2018-07-29 18:04:46 -0700

    Activity test

--- test/src/Activity.test.js
+++ test/src/Activity.test.js
@@ -1,13 +1,25 @@
 'use strict'
 
 const Activity = require('../../src/Activity.js')
-const AR = new ActiveRules()
 const assert = require('assert')
 
 describe('Activity', function () {
-  describe('Does stuff', function () {
-    it('oh yeah', function () {
-      return true
+  describe('extends ActiveRulesObject', function () {
+    
+    const o = new Activity();
+    const parent = Object.getPrototypeOf(Object.getPrototypeOf(o)).constructor.name
+    const className = o.constructor.name
+    
+    it('is an object', function () {
+      assert.equal(typeof o, 'object')
+    })
+    
+    it('is named ActiveRulesError', function () {
+      assert.equal(className, 'Activity')
+    })
+    
+    it('is a child of ActiveRulesObject', function () {
+      assert.equal(parent, 'ActiveRulesObject')
     })
   })
 })
