6460ba2a21450623fd3ed013fdb60fb6dcd7c178
Brian Winkers <bwinkers@gmail.com>
2018-07-29 18:05:20 -0700

    use describe not it

--- test/src/ActiveRulesError.test.js
+++ test/src/ActiveRulesError.test.js
@@ -4,22 +4,22 @@ const ActiveRulesError = require('../../src/ActiveRulesError.js')
 const assert = require('assert')
 
 describe('ActiveRulesError', function () {
-  it('extends ActiveRulesObject', function () {
+  describe('extends ActiveRulesObject', function () {
     
     const o = new ActiveRulesError();
     const parent = Object.getPrototypeOf(Object.getPrototypeOf(o)).constructor.name;
-    const className = o.constructor.name;
+    const className = o.constructor.name
     
     it('is an object', function () {
       assert.equal(typeof o, 'object');
-    });
+    })
     
     it('is named ActiveRulesError', function () {
       assert.equal(className, 'ActiveRulesError');
-    });
+    })
     
     it('is a child of ActiveRulesObject', function () {
       assert.equal(parent, 'ActiveRulesObject');
-    });
+    })
   })
 })
