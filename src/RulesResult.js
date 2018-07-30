b51fb9147ebeb8cf9299ce280303521d28d68e71
Brian Winkers <bwinkers@gmail.com>
2018-07-29 18:09:52 -0700

    Extend ActiveRulesObject

--- src/RulesResult.js
+++ src/RulesResult.js
@@ -2,17 +2,15 @@
  * A results object created by prcoessing a date with rules.
  */
 
-const ARObject = require('./ActiveRulesObject.js')
+const ActiveRulesObject = require('./ActiveRulesObject.js')
 
 /**
  * Defines ActiveRules RulesResult functionality
  */
-class RulesResult extends ARObject {
-  
-  constructor () {
-
+class RulesResult extends ActiveRulesObject {
+  constructor(...args) {
+    super(...args)
   }
-  
 }
 
 module.exports = RulesResult
