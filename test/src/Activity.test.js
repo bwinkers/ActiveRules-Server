c5bdb35cb3ff9218fa4cd3ab43727d2b626bdbce
Brian Winkers <bwinkers@gmail.com>
2018-07-29 17:58:36 -0700

    Activity test stub

---
+++ test/src/Activity.test.js
@@ -0,0 +1,13 @@
+'use strict'
+
+const Activity = require('../../src/Activity.js')
+const AR = new ActiveRules()
+const assert = require('assert')
+
+describe('Activity', function () {
+  describe('Does stuff', function () {
+    it('oh yeah', function () {
+      return true
+    })
+  })
+})
